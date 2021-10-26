c2d4d65e040cd5525ff0ee2351e4cc4319d02f5d
Nilan Bais <nilanbais@outlook.com>
2021-10-26 17:07:23 +0200

    Update sandbox.py

--- server_files/sandbox.py
+++ server_files/sandbox.py
@@ -1,4 +1,45 @@
-set_1 = {'91', '13', '23', '30', '63', '46B', '17', '41', '61'}
-set_2 = {'91', '13', '23', '30', '63', '46B', '17', '41', '61'}
+"""
+test_template has next vars:
+name
+function
+adres
+tel
+e-mail
+tekst
+row_contents
+"""
+import os
+import docxtpl
 
-x = set_2.difference(set_1)
+tekst = """
+multi line string vol met bullshiii puur om te testen met 
+een 
+multi 
+line 
+string.
+"""
+
+data_package = {"name": "young baisie",
+                "function": "Big Man",
+                "adres": "On the Block",
+                "tel": "0223 ja toch",
+                "mail": "bisoe@skrt.io",
+                "tekst": tekst,
+                "row_contents": [
+                    {"description": "a b c",
+                     "value": "ja",
+                     "bool": True},
+                    {"description": "a b c",
+                     "value": "nee",
+                     "bool": True},
+                    {"description": "a b c",
+                     "value": "prrt",
+                     "bool": True}
+                ]
+                }
+
+print(os.getcwd())
+
+doc = docxtpl.DocxTemplate("test_template_2.docx")
+doc.render(data_package)
+doc.save("test_render.docx")
